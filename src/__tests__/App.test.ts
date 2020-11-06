c026af4695c0fcbbd64c731a46a76851076662fe
Debreczeni Kálmán <debreczeni.kalman14@gmail.com>
2020-11-06 21:42:03 +0100

    AddTests

--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -1,15 +1,66 @@
-test("hello world", () => {
-    const str = "Hello, WORLD!";
-    expect(typeof str).toBe("string");
-    expect(str.toLocaleLowerCase()).toBe("hello, world!");
-    expect(str.split(" ")[0]).toBe("Hello,");
-    expect(str.split(" ")[1]).toBe("WORLD!");
-});
+import { existsSync } from "fs";
+import Megoldás from "../Megoldás";
 
-test("fibonacci series", () => {
-    const array: number[] = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
-    expect(array).toBeInstanceOf(Array);
-    expect(array.every(n => n > 0)).toBe(true);
-    expect(array).toContain(1);
-    expect(array).toContain(2);
+// test("hello world", () => {
+//     const str = "Hello, WORLD!";
+//     expect(typeof str).toBe("string");
+//     expect(str.toLocaleLowerCase()).toBe("hello, world!");
+//     expect(str.split(" ")[0]).toBe("Hello,");
+//     expect(str.split(" ")[1]).toBe("WORLD!");
+// });
+const feladat: Megoldás = new Megoldás("numberquest.txt");
+test("Helyes beolvasás", () => {
+    expect(feladat.kérdések.length).toBe(feladat.ElsőFeladat());
+});
+test("Helyes minimum és maximum", () => {
+    expect(feladat.kérdésekMin).toBeLessThan(feladat.kérdésekMax);
+    let min = Number.MAX_VALUE;
+    for (let i = 0; i < feladat.kérdések.length; i++) {
+        const e = feladat.kérdések[i];
+        if (e.megoldSzám < min) {
+            min = e.megoldSzám;
+        }
+    }
+    expect(feladat.kérdésekMin).toBe(min);
+    let max = Number.MIN_VALUE;
+    for (let i = 0; i < feladat.kérdések.length; i++) {
+        const e = feladat.kérdések[i];
+        if (e.megoldSzám > max) {
+            max = e.megoldSzám;
+        }
+    }
+
+    expect(feladat.kérdésekMax).toBe(max);
+});
+test("Témák listázása", () => {
+    expect(feladat.ÖtödikFeladat().length).toBe(5);
+    let ismétlődik = false;
+    for (let i = 0; i < feladat.ÖtödikFeladat().length; i++) {
+        const e = feladat.ÖtödikFeladat()[i];
+        for (let j = i + 1; j < feladat.ÖtödikFeladat().length; j++) {
+            const ee = feladat.ÖtödikFeladat()[j];
+            if (e == ee) {
+                ismétlődik = true;
+            }
+        }
+    }
+    expect(ismétlődik).toBe(false);
+});
+test("Megfelelő pontozás", () => {
+    let min = Number.MAX_VALUE;
+    let max = Number.MIN_VALUE;
+    for (let i = 0; i < feladat.kérdések.length; i++) {
+        const e = feladat.kérdések[i].pontszám;
+        if (min > e) {
+            min = e;
+        } else if (max < e) {
+            max = e;
+        }
+    }
+    expect(min).toBe(1);
+    expect(max).toBe(3);
+});
+test("Létező kimenet", () => {
+    feladat.HetedikFeladat();
+    expect(existsSync("testquests.txt")).toBe(true);
 });
